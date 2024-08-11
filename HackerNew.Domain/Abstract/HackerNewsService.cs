440c3cc9aaeca13948918d480104e411d59120a7
rsi-gaurav <gaurav.agarwal1@rsystems.com>
2024-08-11 07:47:53 -0500

    Add files via upload

--- HackerNew.Domain/Abstract/HackerNewsService.cs
+++ HackerNew.Domain/Abstract/HackerNewsService.cs
@@ -1,10 +1,6 @@
 ﻿using HackerNews.Domain.DTO;
 using HackerNews.Domain.Interface;
-using HackerNews.Domain.Constants;
 using Microsoft.Extensions.Caching.Memory;
-using System.Text.Json;
-using System.Net;
-using System.Threading.Tasks;
 
 namespace HackerNews.Domain.Abstract
 {
@@ -13,64 +9,49 @@ namespace HackerNews.Domain.Abstract
     /// </summary>
     public class HackerNewsService : IHackerNewsService
     {
-        private readonly IHttpClientFactoryService _httpClientFactoryService;
         private readonly IMemoryCache _memoryCache;
+        private readonly IAPIService _apiService;
         private readonly TimeSpan _cacheDuration = TimeSpan.FromMinutes(5);
         private const string NewStoriesCacheKey = "NewStories";
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HackerNewsService"/> class.
         /// </summary>
-        /// <param name="httpClientFactoryService">The HTTP client factory service.</param>
         /// <param name="memoryCache">The memory cache.</param>
-        public HackerNewsService(IHttpClientFactoryService httpClientFactoryService, IMemoryCache memoryCache)
+        /// <param name="apiService">The API service.</param>
+        public HackerNewsService(IMemoryCache memoryCache, IAPIService apiService)
         {
-            _httpClientFactoryService = httpClientFactoryService;
             _memoryCache = memoryCache;
+            _apiService = apiService;
         }
 
         /// <summary>
-        /// Retrieves the list of new stories from Hacker News.
+        /// Retrieves the new stories from Hacker News.
         /// </summary>
-        /// <returns>The list of new stories.</returns>
+        /// <returns>A list of <see cref="HackerNewsDTO"/> representing the new stories.</returns>
         public async Task<List<HackerNewsDTO>> GetNewStoriesAsync()
         {
-
             if (_memoryCache.TryGetValue(NewStoriesCacheKey, out List<HackerNewsDTO>? hackernewslist))
             {
                 return hackernewslist;
             }
+            hackernewslist = new List<HackerNewsDTO>(0);
+            var storiesIDes = (await _apiService.GetAllStoriesIds()).Take(200);
 
-            var client = _httpClientFactoryService.CreateClient();
-
-            // Fetch the top stories
-            var response = await client.GetAsync(ApiUrls.NewStories);
-            response.EnsureSuccessStatusCode();
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            // Deserialize the list of story IDs
-            var storyIds = JsonSerializer.Deserialize<List<int>>(responseContent);
-
-            hackernewslist = new List<HackerNewsDTO>();
-            // Fetch details for each story in parallel
-            Parallel.ForEach(storyIds.Take(200), id =>
+            Parallel.ForEach(storiesIDes, id =>
             {
-                var storyResponse = client.GetAsync(string.Format(ApiUrls.StoryDetails, id));
-                var storyContent = storyResponse.Result.Content.ReadAsStringAsync();
-                HackerNewsDTO? story = JsonSerializer.Deserialize<HackerNewsDTO>(storyContent.Result);
-                if (story != null)
+                var tasks = _apiService.GetStoryDetail(id);
+                if (tasks != null)
                 {
-                    hackernewslist.Add(story);
+                    hackernewslist.Add(tasks.Result);
                 }
             });
             hackernewslist = hackernewslist.OrderByDescending(o => o.id).ToList();
-
             var cacheEntryOptions = new MemoryCacheEntryOptions()
                 .SetSlidingExpiration(_cacheDuration)
                 .SetAbsoluteExpiration(_cacheDuration);
 
             _memoryCache.Set(NewStoriesCacheKey, hackernewslist, cacheEntryOptions);
-
             return hackernewslist;
         }
     }
