37d119a504530e48069672961315bf7ce2509bc8
rsi-gaurav <gaurav.agarwal1@rsystems.com>
2024-08-11 07:47:25 -0500

    Add files via upload

---
+++ HackerNew.Domain/Interface/IAPIService.cs
@@ -0,0 +1,23 @@
+﻿using HackerNews.Domain.DTO;
+
+namespace HackerNews.Domain.Interface
+{
+    /// <summary>
+    /// Represents an API service for retrieving Hacker News data.
+    /// </summary>
+    public interface IAPIService
+    {
+        /// <summary>
+        /// Retrieves all the story IDs from Hacker News.
+        /// </summary>
+        /// <returns>An asynchronous operation that returns a collection of story IDs.</returns>
+        Task<IEnumerable<int>> GetAllStoriesIds();
+
+        /// <summary>
+        /// Retrieves the details of a specific story from Hacker News.
+        /// </summary>
+        /// <param name="StoryId">The ID of the story to retrieve.</param>
+        /// <returns>An asynchronous operation that returns the details of the story.</returns>
+        Task<HackerNewsDTO> GetStoryDetail(int StoryId);
+    }
+}
--- HackerNew.Domain/Interface/IHackerNewsService.cs
+++ HackerNew.Domain/Interface/IHackerNewsService.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace HackerNews.Domain.Interface
 {
+    /// <summary>
+    /// Represents the interface for the Hacker News service.
+    /// </summary>
     public interface IHackerNewsService
     {
+        /// <summary>
+        /// Retrieves a list of new stories from Hacker News asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the list of new stories.</returns>
         Task<List<HackerNewsDTO>> GetNewStoriesAsync();
     }
 }
