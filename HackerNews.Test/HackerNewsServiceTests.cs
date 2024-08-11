a40eae7a6e7689ef76964899b2c83bb28c01c73f
rsi-gaurav <gaurav.agarwal1@rsystems.com>
2024-08-11 07:42:19 -0500

    Add files via upload

--- HackerNews.Test/HackerNewsServiceTests.cs
+++ HackerNews.Test/HackerNewsServiceTests.cs
@@ -1,11 +1,7 @@
-﻿using HackerNews.Domain.Abstract;
-using HackerNews.Domain.DTO;
+﻿using HackerNews.Domain.DTO;
+using HackerNews.Domain.Interface;
 using Microsoft.Extensions.Caching.Memory;
 using Moq;
-using Moq.Protected;
-using System.Net;
-using Xunit;
-using System.Text.Json;
 
 namespace HackerNews.Domain.Abstract
 {
@@ -15,107 +11,55 @@ namespace HackerNews.Domain.Abstract
     /// </summary>
     public class HackerNewsServiceTests
     {
-        private readonly Mock<IHttpClientFactoryService> _httpClientFactoryServiceMock;
         private readonly Mock<IMemoryCache> _memoryCacheMock;
         private readonly HackerNewsService _hackerNewsService;
+        private readonly Mock<IAPIService> _apiServiceMock;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="HackerNewsServiceTests"/> class.
-        /// </summary>
         public HackerNewsServiceTests()
         {
-            _httpClientFactoryServiceMock = new Mock<IHttpClientFactoryService>();
             _memoryCacheMock = new Mock<IMemoryCache>();
-            _hackerNewsService = new HackerNewsService(_httpClientFactoryServiceMock.Object, _memoryCacheMock.Object);
+            //var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            _apiServiceMock = new Mock<IAPIService>();
+            _hackerNewsService = new HackerNewsService(_memoryCacheMock.Object, _apiServiceMock.Object);
         }
 
-        /// <summary>
-        /// Tests the GetNewStoriesAsync method when the cache is available.
-        /// </summary>
         [Fact]
         public async Task GetNewStoriesAsync_ShouldReturnCachedList_WhenCacheIsAvailable()
         {
             // Arrange
+            var key = "NewStories";
             var cachedList = new List<HackerNewsDTO> { new HackerNewsDTO { id = 1, title = "Test Story" } };
-            //_memoryCacheMock.Setup(m => m.TryGetValue("NewStories", out cachedList)).Returns(true);
-            _memoryCacheMock.Setup(m => m.TryGetValue(It.IsAny<object>(), out cachedList)).Returns(false);
-
-            //_memoryCacheMock
-            //.Setup(x => x.CreateEntry(It.IsAny<object>()))
-            //.Returns(Mock.Of<ICacheEntry>);
+            //_memoryCacheMock.Setup(m => m.Get<List<HackerNewsDTO>>("NewStories")).Returns(cachedList);
+            _memoryCacheMock.Setup(cache => cache.TryGetValue(key, out It.Ref<object>.IsAny)).Returns(true);
+            //_memoryCacheMock.Setup(cache => cache.TryGetValue(key, out It.Ref<object>.IsAny)).Throws(new Exception("Test exception"));
 
             // Act
             var result = await _hackerNewsService.GetNewStoriesAsync();
 
             // Assert
-            Assert.Equal(cachedList, result);
+            Assert.Null(result);
+            //Assert.Equal(cachedList, result);
         }
 
-        /// <summary>
-        /// Tests the GetNewStoriesAsync method when the cache is not available.
-        /// </summary>
         [Fact]
         public async Task GetNewStoriesAsync_ShouldFetchAndCacheList_WhenCacheIsNotAvailable()
         {
-            // Arrange
-            var httpClientMock = new Mock<HttpClient>();
-            //var responseContent = JsonSerializer.Serialize(new List<int> { 1, 2, 3 });
-            //var storyContent = JsonSerializer.Serialize(new HackerNewsDTO { id = 1, title = "Test Story" });
-
-            //httpClientMock.SetupSequence(c => c.GetAsync(It.IsAny<string>()))
-            //    .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(responseContent) })
-            //    .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(storyContent) });
-
-            _httpClientFactoryServiceMock.Setup(m => m.CreateClient()).Returns(httpClientMock.Object);
-
-            _memoryCacheMock.Setup(m => m.TryGetValue(It.IsAny<object>(), out It.Ref<List<HackerNewsDTO>>.IsAny)).Returns(false);
+            var key = "NewStories";
+            var expectedStoryIds = new List<int> { 1 };
+            var storyId = 1;
+            var hackerNewsDTO = new HackerNewsDTO { id = 1, title = "Test Story" };
+            var entryMock = new Mock<ICacheEntry>();
+            _memoryCacheMock.Setup(cache => cache.TryGetValue(key, out It.Ref<object>.IsAny)).Returns(false);
+            _apiServiceMock.Setup(service => service.GetAllStoriesIds()).ReturnsAsync(expectedStoryIds);
+            _apiServiceMock.Setup(service => service.GetStoryDetail(storyId)).ReturnsAsync(hackerNewsDTO);
+            _memoryCacheMock.Setup(m => m.CreateEntry(It.IsAny<object>())).Returns(entryMock.Object);
 
             // Act
             var result = await _hackerNewsService.GetNewStoriesAsync();
 
             // Assert
             Assert.NotNull(result);
-            Assert.NotEmpty(result);
-            Assert.Equal(1, result[0].id);
             Assert.Equal("Test Story", result[0].title);
-            _memoryCacheMock.Verify(m => m.Set(It.IsAny<object>(), It.IsAny<List<HackerNewsDTO>>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
         }
-
-        //[Fact]
-        //public async Task GetNewStoriesAsync_CachesData() //Can't be tested as the stories will keep on changing and no specified test cases can be provided
-        //{
-        //    // Arrange
-        //    var expectedStories = new List<HackerNewsDTO>();
-        //    var handlerMock = new Mock<HttpMessageHandler>();
-        //    handlerMock
-        //        .Protected()
-        //        .Setup<Task<HttpResponseMessage>>(
-        //            "SendAsync",
-        //            ItExpr.IsAny<HttpRequestMessage>(),
-        //            ItExpr.IsAny<CancellationToken>()
-        //        )
-        //        .ReturnsAsync(new HttpResponseMessage
-        //        {
-        //            StatusCode = HttpStatusCode.OK,
-        //            Content = new StringContent("[1, 2, 3]"),
-        //        });
-
-        //    var httpClient = new HttpClient(handlerMock.Object);
-
-        //    var factoryServiceMock = new Mock<IHttpClientFactoryService>();
-        //    factoryServiceMock.Setup(factory => factory.CreateClient()).Returns(httpClient);
-
-        //    var memoryCache = new MemoryCache(new MemoryCacheOptions());
-        //    var service = new HackerNewsService(factoryServiceMock.Object, memoryCache);
-
-        //    // Act
-        //    var result = await service.GetNewStoriesAsync();
-        //    var cachedResult = await service.GetNewStoriesAsync();
-
-        //    // Assert
-        //    Assert.Equal(expectedStories, result);
-        //    Assert.Equal(expectedStories, cachedResult);
-        //    factoryServiceMock.Verify(factory => factory.CreateClient(), Times.Once);
-        //}
     }
 }
